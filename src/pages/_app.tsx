6a3f8a41233e31aa178a4fd371460eb1156e3bb2
Rodrigo <rodrigorael53@gmail.com>
2023-02-08 02:40:32 -0300

    :sparkles: feat: add axe core react

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
+import { axeAccessibilityReporter } from "@/utils/axeAccessibilityReporter";
+import { useEffect } from "react";
+
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    axeAccessibilityReporter();
+  }, []);
+
   return <Component {...pageProps} />;
 }
